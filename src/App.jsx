2e9e4e85b12439b1b77a590f8e5dc00c25a45edf
Carlos <carloslcgt@gmail.com>
2023-03-27 12:30:58 -0700

    proyecto en blanco

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/react-in-jsx-scope */
+//import { useState } from 'react'
+import Clientes from "./Clientes"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Clientes />
+    </div>
+  )
+}
+
+export default App
